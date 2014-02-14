c03aee0b413e140185dc98af2e785c5a864b560d
Derrick Showers <derrick_showers@apple.com>
2014-02-14 15:39:14 -0800

    changing server address

--- public/js/server.js
+++ public/js/server.js
@@ -3,6 +3,6 @@ var server = {};
 if (document.URL.indexOf('localhost') > 0) {
   server.serverAddress = 'http://localhost:8080';
 } else {
-  server.serverAddress = 'http://dsnodechat.herokuapp.com/'; 
+  server.serverAddress = 'http://chat.derrickshowers.com/'; 
 }
 server = io.connect(server.serverAddress);
